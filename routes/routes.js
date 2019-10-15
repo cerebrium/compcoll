682eaac864ee5dd2fb462d0d09d6765bf56e925b
cerebrium <nicholas.m.shankland@gmail.com>
2019-10-15 15:35:21 -0700

    working on regex

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
-
+const axios = require('axios');
+const cheerio = require('cheerio');
+const $ = cheerio.load(`https://www.breitbart.com/politics/`);
 
 router.get(`/`, function(req, res) {
-    res.render('home')
-})
+    axios.get(`https://www.breitbart.com/politics/`)
+        .then(function(response) {
+            let myReg = /href="\/\d?\/"/;
+            let myResponse = response.data.toString();
+            let myStrings = myResponse.match(myReg)
+            console.log(myStrings)
+            res.render('scraper')
+        })
+        .catch(function(err) {
+            console.log('---------------------------------------------------------')
+            console.log(err)
+        })
+    })
 
+router.get(`/results`, function(req, res) {
+    res.render('results')
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
+// requires
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const layouts = require('express-ejs-layouts');
 
+// sets
 app.set('view engine', 'ejs');
 
+// uses
+app.use(express.static('static'))
 app.use(layouts)
 
-app.use(`/`, require(`./routes/routes`))
+// gets
+app.get(`/`, function(req, res) {
+    res.render('home')
+})
+
+// app.uses
+app.use(`/compcoll`, require(`./routes/routes`))
 // MIDDLE WARE
 // app.use(express.urlencoded({extended: false}))
 
+
+
+// listeners
 app.listen(3007, function() {
     console.log('Server Is Running: CompCall')
 })
