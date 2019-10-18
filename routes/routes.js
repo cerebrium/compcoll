cd6a7861c0979b02043b46f376b30b42fe98498f
cerebrium <nicholas.m.shankland@gmail.com>
2019-10-18 12:08:44 -0700

    working on some asynchronous stuff

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const cheerio = require('cheerio');
-const $ = cheerio.load(`https://www.breitbart.com/politics/`);
+const async = require('async')
+const newsSites = [
+`https://www.breitbart.com/politics/`,
+'https://www.bbc.com/news/world/us_and_canada',
+'https://www.nytimes.com/section/politics',
+'https://www.infowars.com/category/us-news/',
+'https://www.nbcnews.com/politics'
+]
 
 router.get(`/`, function(req, res) {
-    axios.get(`https://www.breitbart.com/politics/`)
-        .then(function(response) {
-            let myReg = /href="\/\d?\/"/;
-            let myResponse = response.data.toString();
-            let myStrings = myResponse.match(myReg)
-            console.log(myStrings)
-            res.render('scraper')
-        })
-        .catch(function(err) {
-            console.log('---------------------------------------------------------')
-            console.log(err)
-        })
+    res.render('scraper')
     })
 
-router.get(`/results`, function(req, res) {
-    res.render('results')
+router.post(`/results`, function(req, res) {
+    console.log('---------------------------------------------------------')
+    console.log('post')
+    res.redirect('/compcoll/results')
 })
+    
+router.get('/results', function(req, res) {
+    let myData = [];
+    let async_One = function(cb) {
+        newsSites.forEach(function(ele) {
+            axios.get(ele)
+                .then(function(response) {
+                    myData.push((response.data.match(/href=/)).toString())
+                    console.log('Inside of the then loop: ' + myData)
+                })
+            })
+        cb(null, myData)
+    }
+    async.series([async_One], function(err, result) {
+        console.log(result)
+        console.log('async entered last part')
+        res.render('results', {results: result})
+        }) 
+})
+
 module.exports = router;
